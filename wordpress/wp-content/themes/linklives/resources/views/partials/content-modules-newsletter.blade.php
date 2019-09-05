f3a6e90812e1904fa61e28bf9c149475926fd668
Mads Hostrup Mortensen <mm@mm-mac2.lan>
2019-09-05 17:47:15 +0200

    Newsletter forms in module

--- wordpress/wp-content/themes/linklives/resources/views/partials/content-modules-newsletter.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/partials/content-modules-newsletter.blade.php
@@ -6,44 +6,52 @@
     @if(get_sub_field('modules_newsletter_type') == 'true')
 
       <h3>Tilmeld</h3>
-      <!-- BEGYND: HTML-formular  -->
-      <form action="https://testdette.uxmail.io/handlers/post/" method="post">
+¨
+      <form action="https://link-lives.uxmail.io/handlers/post/" method="post">
 
         <input type="hidden" name="action" value="subscribe" />
-        <input type="hidden" name="lists" value="57246" />
+        <input type="hidden" name="lists" value="59269" />
 
         <p>
-          <label>E-mail-adresse</label>
+          <label for="email_address_id">E-mail-adresse</label>
           <input type="text" name="email_address" id="email_address_id" />
+        </p>
+
+        <p>
+          <label for="data_Navn_id">Navn</label>
+          <input type="text" name="data_Navn" id="data_Navn_id" />
         </p>
 
         <p>
           <input type="submit" value="Tilmeld" />
         </p>
 
+        <input type="hidden" name="succes_url" value="https://www.link-lives.dk/du-er-nu-tilmeldt-nyhedsbrevet"/>
+        <input type="hidden" name="failure_url" value="https://www.link-lives.dk/noget-gik-galt"/>
+
       </form>
-      <!-- AFSLUT: HTML-formular  -->
 
     @else
       <h3>Frameld</h3>
 
-      <!-- BEGYND: HTML-formular  -->
-
-      <form action="https://testdette.uxmail.io/handlers/post/" method="post">
+      <form action="https://link-lives.uxmail.io/handlers/post/" method="post">
         <input type="hidden" name="action" value="unsubscribe" />
-        <input type="hidden" name="lists" value="57246" />
+        <input type="hidden" name="lists" value="59269" />
 
         <p>
-          <label>E-mail-adresse</label>
-          <input type="text" name="email_address" id="email_address_id2" />
+          <label for="email_address_id">E-mail-adresse</label>
+          <input type="text" name="email_address" id="email_address_id" />
         </p>
 
         <p>
           <input type="submit" value="Frameld" />
         </p>
 
+      <input type="hidden" name="succes_url" value="https://www.link-lives.dk/du-er-nu-frameldt-nyhedsbrevet/"/>
+      <input type="hidden" name="failure_url" value="https://www.link-lives.dk/noget-gik-galt"/>
+
       </form>
-      <!-- AFSLUT: HTML-formular  -->
+
     @endif
 
   </div>
