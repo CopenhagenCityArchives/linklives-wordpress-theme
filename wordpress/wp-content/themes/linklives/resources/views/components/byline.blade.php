5f8dd836f8594c20a92fc0d05fa5167e3acc352e
madshostrup <hello@madshostrup.dk>
2020-02-02 20:36:42 +0100

    Vertical align member profile image and text top

--- wordpress/wp-content/themes/linklives/resources/views/components/byline.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/components/byline.blade.php
@@ -3,11 +3,11 @@
   $id = isset($id) ? $id : get_the_ID();
 @endphp
 
-<div class="byline align-items-center {{$flex ? $flex : 'd-flex'}}">
+<div class="byline align-items-top {{isset($flex) ? $flex : 'd-flex'}}">
 
   @include('components.profile-image', ['id' => $id, 'class' => 'mr-3'])
 
-  <div>
+  <div class="d-flex flex-column">
     <div class="light">
       {{ get_field('members_title', $id) ? get_field('members_title', $id) : pll__('Medlem') }}
     </div>
