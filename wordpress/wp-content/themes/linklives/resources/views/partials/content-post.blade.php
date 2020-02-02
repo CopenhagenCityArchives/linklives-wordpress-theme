3d0a24d491bc595b1a9efb6dbb043f441f51d9ef
madshostrup <hello@madshostrup.dk>
2020-02-02 22:29:41 +0100

    No stretching of featured images

--- wordpress/wp-content/themes/linklives/resources/views/partials/content-post.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/partials/content-post.blade.php
@@ -1,9 +1,16 @@
 <a {{ post_class($classes . ' post-type') }} href="{{ get_the_permalink() }}">
   <article class="d-flex align-items-stretch" aria-label="{{ pll__('Nyhed') . ' – ' . get_the_title()}}">
     @if ( has_post_thumbnail() )
-      @include('components.thumbnail', ['size' => 'thumbnail-image-x1'])
+      <div class="thumbnail-bg" style="background-image: url({{ get_the_post_thumbnail_url(null, 'thumbnail-image-x1') }})" /></div>
+      {{-- <div class="thumbnail-wrapper">
+        @include('components.thumbnail', ['size' => 'thumbnail-image-x1'])
+      </div> --}}
     @endif
     <div class="content">
+      @if ( has_post_thumbnail() )
+        <div class="pl-sm-4 pl-lg-0 pl-xl-4 pt-2 pt-sm-0 pt-lg-2 pt-xl-0">
+      @endif
+
 
       <time datetime="{{ get_post_time('c') }}">{{ get_the_date(pll__( 'j. M Y' )) }}</time>
       <header>
@@ -21,6 +28,11 @@
         </div>
       @endif
       @include('components.icon', ['icon' => 'arrow-right', 'class' => 'right'])
+
+      @if ( has_post_thumbnail() )
+       </div>
+      @endif
+
     </div>
   </article>
 </a>
