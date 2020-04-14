c3a6a8c8e5a320d13b9725205987e70fa0b174a5
Bo Henriksen <stumpdenmark@hotmail.com>
2020-04-14 13:31:19 +0200

    Update head.blade.php

--- wordpress/wp-content/themes/linklives/resources/views/partials/head.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/partials/head.blade.php
@@ -1,8 +1,6 @@
 <head>
-  <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="401abcfd-34f0-41ec-88b9-ed1d801490a1" data-blockingmode="auto" type="text/javascript"></script>
-  
   <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149577576-1"></script> 
+  <!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149577576-1"></script> -->
   <!-- Google Analytics -->
   <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
