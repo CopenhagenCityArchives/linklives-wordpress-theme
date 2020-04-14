389f112b53afb1d03406b6be3011f48b407aea9a
Bo Henriksen <stumpdenmark@hotmail.com>
2020-04-14 14:08:47 +0200

    Update head.blade.php

--- wordpress/wp-content/themes/linklives/resources/views/partials/head.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/partials/head.blade.php
@@ -1,17 +1,17 @@
 <head>
   <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="45617c96-571e-4b06-9521-9a417f327c48" data-blockingmode="auto" type="text/javascript"></script>
+  
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149577576-1"></script> -->
   <!-- Google Analytics -->
   <script type="text/plain" data-cookieconsent="statistics">
-  
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-149577576-1', 'auto');
-  ga('send', 'pageview');
+    ga('create', 'UA-149577576-1', 'auto');
+    ga('send', 'pageview');
   </script>
   <!-- End Google Analytics -->
 
