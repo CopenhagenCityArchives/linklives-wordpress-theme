c26bd50328eee63641a0962b9bc2dd025bb4e2b4
madshostrup <hello@madshostrup.dk>
2020-11-12 20:34:31 +0100

    Custom fields for the search template

--- wordpress/wp-content/themes/linklives/resources/views/template-search.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/template-search.blade.php
@@ -9,11 +9,21 @@
   <app-root></app-root>
 
   <script>
-    window.lls = {featherIconPath: "{{ App\asset_path('images/feather-sprite.svg') }}"};
+    window.lls = {
+      featherIconPath: "{{ App\asset_path('images/feather-sprite.svg') }}",
+      language: "{{ pll_current_language() }}",
+      title: "{{ get_field('search_title_da') }}",
+      titleEn: "{{ get_field('search_title_en') }}",
+      introText: "{{ get_field('search_lead_da') }}",
+      introTextEn: "{{ get_field('search_lead_en') }}",
+      helpBoxText: "{{ get_field('search_help_da') }}",
+      helpBoxTextEn: "{{ get_field('search_help_en') }}"
+    };
   </script>
   <style>
-  @import url("https://dx7i5z2o95p3n.cloudfront.net/frontend/styles.css");<br />
-  </style><script src="https://dx7i5z2o95p3n.cloudfront.net/frontend/runtime-es2015.js" type="module"></script>
+    @import url("https://dx7i5z2o95p3n.cloudfront.net/frontend/styles.css");
+  </style>
+  <script src="https://dx7i5z2o95p3n.cloudfront.net/frontend/runtime-es2015.js" type="module"></script>
   <script src="https://dx7i5z2o95p3n.cloudfront.net/frontend/runtime-es5.js" nomodule="" defer=""></script>
   <script src="https://dx7i5z2o95p3n.cloudfront.net/frontend/polyfills-es5.js" nomodule="" defer=""></script>
   <script src="https://dx7i5z2o95p3n.cloudfront.net/frontend/polyfills-es2015.js" type="module"></script>
