e641becfbd089bcceae9e9e4044eaec3f1f74bb2
madshostrup <hello@madshostrup.dk>
2022-03-24 13:20:41 +0100

    Fix styling on tag archive

--- wordpress/wp-content/themes/linklives/resources/views/archive-fond.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/archive-fond.blade.php
@@ -1,1 +1,1 @@
-@include('archive')
+@include('archive-partner')
--- wordpress/wp-content/themes/linklives/resources/views/archive.blade.php
+++ wordpress/wp-content/themes/linklives/resources/views/archive.blade.php
@@ -1,18 +1,1 @@
-@extends('layouts.app')
-
-@section('content')
-
-  @include('partials.page-header')
-
-  <section class="module archive-wrapper theme-{{ get_field('theme', get_post_type() . '_options') ? get_field('theme', get_post_type() . '_options') : 'green' }}">
-    <div class="container-fluid">
-      <div class="row row-eq-height">
-        @while (have_posts()) @php the_post() @endphp
-          @include('partials.content-partner', ['classes' => 'col-lg-6'])
-        @endwhile
-      </div>
-    </div>
-
-  </section>
-
-@endsection
+@include('index')
